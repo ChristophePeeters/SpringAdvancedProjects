a42c54d2e307321ee42e956a33b5d212830557f2
christophe.peeters <peeters1993@msn.com>
2020-02-19 10:53:49 +0100

    JMS

--- groenetenen/src/main/java/be/vdab/groenetenen/mail/DefaultMailSender.java
+++ groenetenen/src/main/java/be/vdab/groenetenen/mail/DefaultMailSender.java
@@ -4,6 +4,7 @@ import be.vdab.groenetenen.entities.Offerte;
 import be.vdab.groenetenen.exceptions.KanMailNietZendenException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -17,9 +18,12 @@ import javax.mail.internet.MimeMessage;
 class DefaultMailSender implements MailSender {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final JavaMailSender sender;
+    private final String emailAdresWebMaster;
 
-    DefaultMailSender(JavaMailSender sender) {
+    DefaultMailSender(JavaMailSender sender,
+                      @Value("${emailAdresWebMaster}") String emailAdresWebMaster) {
         this.sender = sender;
+        this.emailAdresWebMaster = emailAdresWebMaster;
     }
 
     @Override
@@ -31,11 +35,26 @@ class DefaultMailSender implements MailSender {
             helper.setTo(offerte.getEmailAdres());
             helper.setSubject("Nieuwe offerte");
             String offerteURL = offertesURL + offerte.getId();
-            helper.setText("Uw offerte heeft het nummer <strong>"+offerte.getId()+"</strong>."+
-                    "Je vindt de offerte <a href='" + offerteURL + "'>hier</a>.",true);
+            helper.setText("Uw offerte heeft het nummer <strong>" + offerte.getId() + "</strong>." +
+                    "Je vindt de offerte <a href='" + offerteURL + "'>hier</a>.", true);
             sender.send(message);
         } catch (MessagingException | MailException ex) {
             logger.error("Kan mail nieuwe offerte niet versturen", ex);
+            throw new KanMailNietZendenException();
+        }
+    }
+
+    @Override
+    public void aantalOffertesMail(long aantal) {
+        try {
+            MimeMessage message = sender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setTo(emailAdresWebMaster);
+            helper.setSubject("Aantal offertes");
+            helper.setText("Aantal offertes:<strong>" + aantal + "</strong>", true);
+            sender.send(message);
+        } catch (MessagingException | MailException ex) {
+            logger.error("Kan mail aantal offertes niet versturen", ex);
             throw new KanMailNietZendenException();
         }
     }
--- groenetenen/src/main/java/be/vdab/groenetenen/mail/MailSender.java
+++ groenetenen/src/main/java/be/vdab/groenetenen/mail/MailSender.java
@@ -3,4 +3,6 @@ package be.vdab.groenetenen.mail;
 import be.vdab.groenetenen.entities.Offerte;
 
 public interface MailSender {
-    void nieuweOfferte(Offerte offerte, String offertesURL);}
+    void nieuweOfferte(Offerte offerte, String offertesURL);
+    void aantalOffertesMail(long aantal);
+}
